85c9fd7c5c55a25021e2b3ad6a52ff8533ec2e0f
Diego Delgado Carrillo <diego@Diegos-MacBook-Air.local>
2023-11-06 13:28:55 -0700

    final

--- flask-cupcakes/app.py
+++ flask-cupcakes/app.py
@@ -1,52 +1,60 @@
-from flask import Flask, jsonify, request
-from models import db, Cupcake
+from flask import Flask, jsonify, request, render_template
+from models import db, Cupcake, connect_db
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "PASSWORD-LEAKED"
 
-db.app = app
-db.init_app(app)
+connect_db(app)
 
-# This route lists all cupcakes, retreives 
-# data from the database and returns it in JSON format
-@app.route('/api/cupcakes', methods=['GET'])
-def list_cupcakes():
-    cupcakes = Cupcake.query.all()
-    cupcake_list = [{'id': cupcake.id, 'flavor': cupcake.flavor, 'size': cupcake.size, 'rating': cupcake.rating, 'image': cupcake.image} for cupcake in cupcakes]
-    return jsonify(cupcakes=cupcake_list)
+@app.route('/')
+def home_page():
+    return render_template('index.html')
 
-# Retreives data about a single cupcake by 
-# it's ID and returns it in JSON format, if not found, returns 404
-@app.route('/api/cupcakes/<int:cupcake_id>', methods=['GET'])
-def get_cupcake(cupcake_id):
-    cupcake = Cupcake.query.get(cupcake_id)
-    if cupcake is None:
-        return jsonify(message="Cupcake not found"), 404
-    return jsonify(cupcake={'id': cupcake.id, 'flavor': cupcake.flavor, 'size': cupcake.size, 'rating': cupcake.rating, 'image': cupcake.image})
+@app.route('/api/cupcakes')
+def list_of_cupcakes():
+    cupcakes = [cupcake.__repr__() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=cupcakes)
 
-# Creates new cupcake using the data from the 
-# request body and returns the created cupcake's data in JSOn format
-@app.route('/api/cupcakes', methods=['POST'])
+@app.route("/api/cupcakes", methods=["POST"])
 def create_cupcake():
     data = request.json
-    flavor = data.get('flavor')
-    size = data.get('size')
-    rating = data.get('rating')
-    image = data.get('image', "https://tinyurl.com/demo-cupcake")
 
-    if not flavor or not size or rating is None:
-        return jsonify(message="Incomplete data. Please provide flavor, size, and rating."), 400
+    cupcake = Cupcake(
+        flavor=data['flavor'],
+        rating=data['rating'],
+        size=data['size'],
+        image=data['image'] or None)
 
-    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
-    db.session.add(new_cupcake)
+    db.session.add(cupcake)
     db.session.commit()
 
-    return jsonify(cupcake={'id': new_cupcake.id, 'flavor': new_cupcake.flavor, 'size': new_cupcake.size, 'rating': new_cupcake.rating, 'image': new_cupcake.image}), 201
+    return (jsonify(cupcake=cupcake.__repr__()), 201)
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+@app.route('/api/cupcakes/<int:cupcake_id>')
+def retreive_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    return jsonify(cupcake=cupcake.__repr__())
 
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=['PATCH'])
+def update_cupcake(cupcake_id):
+    data = request.json
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    cupcake.flavor = data['flavor']
+    cupcake.rating = data['rating']
+    cupcake.size = data['size']
+    cupcake.image = data['image']
+
+    db.session.add(cupcake)
+    db.session.commit()
+    return jsonify(cupcake=cupcake.__repr__())
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=['DELETE'])
+def remove_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    db.session.delete(cupcake)
+    db.session.commit()
+    return jsonify(message='Removed')
